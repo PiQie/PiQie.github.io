e9bec1e75ac89e1323ec7234d181d7f63ae60cbf
PiQie <PiQie@github.com>
2019-06-15 13:23:30 +0200

    background changing and buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,40 @@
-function test() {
-    alert("Hello World!");
+function initTheme() {
+    if (window.sessionStorage) {
+        var btn_changeTheme = document.getElementById('bg_color');
+        btn_changeTheme.addEventListener('click', changeTheme, false);
+
+        if (sessionStorage.themeColor) {
+            var themeColor = sessionStorage.getItem('themeColor');
+            
+            applyTheme(themeColor);
+        }
+    }
+}
+
+function changeTheme() {
+    var bg_color = $('body').css('background-color');
+
+    if (bg_color == 'rgb(255, 255, 255)') {
+        $('body').css('background-color', 'black');
+        $('.white_btn').css('background-color', 'grey');
+        sessionStorage.setItem('themeColor', 'black');
+    } else {
+        $('body').css('background-color', 'white');
+        sessionStorage.setItem('themeColor', 'white');
+    }
+}
+
+function applyTheme() {
+    var themeColor = sessionStorage.getItem('themeColor');
+
+    if (themeColor == 'black') {
+        $('body').css('background-color', 'black');
+    } else {
+        $('body').css('background-color', 'white');
+    }
+}
+
+function init() {
+    var themeColor = sessionStorage.getItem('themeColor');
+    applyTheme(themeColor);
 }
